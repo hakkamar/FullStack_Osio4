d0f813cbf2ab3071a7fdd30fa281061381688269
hakkamar <hakkis68@gmail.com>
2018-08-24 11:17:46 +0300

    4.5 apufunktioita ja yksikkötestejä, osa 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,14 +3,39 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogs) => {  
+const totalLikes = (blogs) => {
   let likesTotal = blogs.reduce(function(sum, blog) {
     return sum + blog.likes
   }, 0)
   return likesTotal
 }
 
+const formatBlog = (blog) => {
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
+const favoriteBlog =(blogs) => {
+  if ( blogs.length === 0 ) {
+    return null
+  }
+
+  let suurin = 0
+  let i = 0
+  for (let index = 0; index < blogs.length; index++) {
+    if (blogs[index].likes > suurin) {
+      suurin = blogs[index].likes
+      i = index
+    }
+  }
+  return formatBlog(blogs[i])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
