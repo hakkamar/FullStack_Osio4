a040c536bc0656f17aac5cdaf7c9336f94b75f86
hakkamar <hakkis68@gmail.com>
2018-09-03 11:06:11 +0300

    4.12 ja 4.13 blogilistan laajennus, osat 1-2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,7 +30,6 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.delete('/:id', async (request, response) => {
   try {
     await Blog.findByIdAndRemove(request.params.id)
-
     response.status(204).end()
   } catch (exception) {
     console.log(exception)
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,89 +2,75 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { initialBlogs, format, nonExistingId, blogsInDb } = require('../utils/blog_api_helper')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+describe('First tests about Blogs', () => {
 
-beforeAll(async () => {
-  await Blog.remove({})
+  beforeAll(async () => {
+    await Blog.remove({})
 
-  for ( let blog of initialBlogs ) {
-    let noteObject = new Blog(blog)
-    await noteObject.save()
-  }
-})
+    for ( let blog of initialBlogs ) {
+      let noteObject = new Blog(blog)
+      await noteObject.save()
+    }
+  })
+  test('all blogs are returned as json by GET /api/blogs', async () => {
+    const blogsInDatabase = await blogsInDb()
 
-test('Blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-describe('Tests about Blogs', () => {
-  test('All Blogs are returned', async () => {
     const response = await api
       .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-    expect(response.body.length).toBe(initialBlogs.length)
+    expect(response.body.length).toBe(blogsInDatabase.length)
+
+    const returnedTitles = response.body.map(n => n.title)
+    blogsInDatabase.forEach(blog => {
+      expect(returnedTitles).toContain(blog.title)
+    })
   })
-  test('a specific blog is within the returned Blogs', async () => {
+  test('a specific blog returned as json by GET /api/blogs/:id', async () => {
+
+    const blogsInDatabase = await blogsInDb()
+    const aBlog = blogsInDatabase[0]
+
     const response = await api
-      .get('/api/blogs')
+      .get(`/api/blogs/${aBlog.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-    const titles = response.body.map(r => r.title)
-    expect(titles).toContainEqual('React patterns')
+    expect(response.body.title).toEqual(aBlog.title)
   })
-  test('a valid blog can be added ', async () => {
+  test('404 returned by GET /api/blogs/:id with nonexisting valid id', async () => {
+    const validNonexistingId = await nonExistingId()
+
+    const response = await api
+      .get(`/api/blogs/${validNonexistingId}`)
+      .expect(404)
+  })
+  test('400 is returned by GET /api/blogs/:id with invalid id', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    const response = await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400)
+  })
+})
+
+describe('Second tests about Blogs', () => {
+
+  beforeAll(async () => {
+    await Blog.remove({})
+
+    for ( let blog of initialBlogs ) {
+      let noteObject = new Blog(blog)
+      await noteObject.save()
+    }
+  })
+
+  test('a valid blog can be added by POST /api/blogs ', async () => {
+    const blogsAtStart = await blogsInDb()
+
     const newBlog = {
       title: 'Lisätty testi Blogi',
       author: 'Testi Henkilö',
@@ -98,75 +84,30 @@ describe('Tests about Blogs', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api
-      .get('/api/blogs')
+    const blogsAfterOperation = await blogsInDb()
 
-    const titles = response.body.map(r => r.title)
-
-    expect(response.body.length).toBe(initialBlogs.length + 1)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+    const titles = blogsAfterOperation.map(r => r.title)
     expect(titles).toContain('Lisätty testi Blogi')
   })
-  test('Blog without content is not added ', async () => {
+  test('a blog without content is NOT added by POST /api/blogs', async () => {
+    const blogsAtStart = await blogsInDb()
+
     const newBlog = {
       likes: 2
     }
-
-    const intialBlogs = await api
-      .get('/api/blogs')
-
     await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(400)
 
-    const response = await api
-      .get('/api/blogs')
+    const blogsAfterOperation = await blogsInDb()
 
-    expect(response.body.length).toBe(intialBlogs.body.length)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
   })
-  test('a specific blog can be viewed', async () => {
-    const resultAll = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  test('a blog withtout likes can be added by POST /api/blogs', async () => {
+    const blogsAtStart = await blogsInDb()
 
-    const aBlogFromAll = resultAll.body[0]
-
-    const resultBlog = await api
-      .get(`/api/blogs/${aBlogFromAll.id}`)
-
-    const blogObject = resultBlog.body
-
-    expect(blogObject).toEqual(aBlogFromAll)
-  })
-  test('a blog can be deleted', async () => {
-    const newBlog = {
-      title: 'HTTP DELETE poistaa resurssin',
-      author: 'Testi Henkilö2',
-      url: 'www.testi2.fi',
-      likes: 0
-    }
-
-    const addedBlog = await api
-      .post('/api/blogs')
-      .send(newBlog)
-
-    const blogsAtBeginningOfOperation = await api
-      .get('/api/blogs')
-
-    await api
-      .delete(`/api/blogs/${addedBlog.body.id}`)
-      .expect(204)
-
-    const blogsAfterDelete = await api
-      .get('/api/blogs')
-
-    const titles = blogsAfterDelete.body.map(r => r.title)
-
-    expect(titles).not.toContain('HTTP DELETE poistaa resurssin')
-    expect(blogsAfterDelete.body.length).toBe(blogsAtBeginningOfOperation.body.length - 1)
-  })
-  test('a blog withtout likes can be added ', async () => {
     const newBlog = {
       title: 'Lisätty Osa 3 testi Blogi',
       author: 'Testi Henkilö 3',
@@ -179,58 +120,86 @@ describe('Tests about Blogs', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api
-      .get('/api/blogs')
+    const blogsAfterOperation = await blogsInDb()
+    const titles = blogsAfterOperation.map(r => r.title)
 
-    const titles = response.body.map(r => r.title)
-
-    // + 2 koska jo aikaisemmin lisättiin yksi.
-    expect(response.body.length).toBe(initialBlogs.length + 2)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
     expect(titles).toContain('Lisätty Osa 3 testi Blogi')
   })
-  test('a blog withtout title can NOT be added ', async () => {
+  test('a blog withtout title can NOT be added by POST /api/blogs', async () => {
+    const blogsAtStart = await blogsInDb()
+
     const newBlog = {
       author: 'Testi Henkilö 4',
       url: 'www.testia4.fi',
       likes: 1
     }
-
     await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(400)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api
-      .get('/api/blogs')
+    const blogsAfterOperation = await blogsInDb()
+    const authors = blogsAfterOperation.map(r => r.author)
 
-    const authors = response.body.map(r => r.author)
-
-    // + 2 koska jo aikaisemmin lisättiin kaksi onnistunutta.
-    expect(response.body.length).toBe(initialBlogs.length + 2)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
     expect(authors).not.toContain('Testi Henkilö 4')
   })
-  test('a blog withtout url can NOT be added ', async () => {
+  test('a blog withtout url can NOT be added by POST /api/blogs', async () => {
+    const blogsAtStart = await blogsInDb()
+
     const newBlog = {
-      title: 'Yritetään lisätä Osa 4 testi Blogi',        
+      title: 'Yritetään lisätä Osa 4 testi Blogi',
       author: 'Testi Henkilö 5',
       likes: 1
     }
-
     await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(400)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api
-      .get('/api/blogs')
+    const blogsAfterOperation = await blogsInDb()
+    const authors = blogsAfterOperation.map(r => r.author)
 
-    const authors = response.body.map(r => r.author)
-
-    // + 2 koska jo aikaisemmin lisättiin kaksi onnistunutta.
-    expect(response.body.length).toBe(initialBlogs.length + 2)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
     expect(authors).not.toContain('Testi Henkilö 5')
+  })
+})
+describe('Third tests about Blogs', () => {
+  beforeEach(async () => {
+    await Blog.remove({})
+
+    for ( let blog of initialBlogs ) {
+      let noteObject = new Blog(blog)
+      await noteObject.save()
+    }
+  })
+  test('DELETE /api/blogs/:id succeeds with proper statuscode', async () => {
+    const newBlog = {
+      title: 'HTTP DELETE poistaa resurssin',
+      author: 'Herra TestiHenkilö',
+      url: 'www.XXXtesti.fi',
+      likes: 0
+    }
+    const addedBlog = await api
+      .post('/api/blogs')
+      .send(newBlog)
+
+    const blogsAtStart = await blogsInDb()
+    const titlesBefore = blogsAtStart.map(r => r.title)
+
+    await api
+      .delete(`/api/blogs/${addedBlog.body.id}`)
+      .expect(204)
+
+    const blogsAfterOperation = await blogsInDb()
+    const titlesAfter = blogsAfterOperation.map(r => r.title)
+
+    expect(titlesBefore).toContain(addedBlog.body.title)
+    expect(titlesAfter).not.toContain(addedBlog.body.title)
+    expect(blogsAfterOperation.length).toBe(blogsAtStart.length - 1)
   })
 })
 
